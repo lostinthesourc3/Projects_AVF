16c8e653d499135b37bdbafddd6d16fb91a85b92
Yuliya Daroshka <yuliyad@fullsail.edu>
2015-10-22 16:13:37 -0400

    WORKING! BUILDING UI FROM DB!

--- Week3YD/build/iphone/build/Products/Debug-iphonesimulator/Week3YD.app/db.js
+++ Week3YD/build/iphone/build/Products/Debug-iphonesimulator/Week3YD.app/db.js
@@ -25,11 +25,11 @@ var save = function(savedData){
 	var db = Ti.Database.open("stockDB");
 	db.execute("CREATE TABLE IF NOT EXISTS stockTBL (id INTEGER PRIMARY KEY, username TEXT, image TEXT, text TEXT)");
 	db.execute("DELETE FROM stockTBL");
-	// for(i=0, j=savedData.length; i<j; i++){
-		// db.execute("INSERT INTO stockTBL (username, image, text) VALUES (?, ?, ?)", savedData[i].username, savedData[i].image, savedData[i].text);
-	// }; 
+	for(i=0, j=savedData.length; i<j; i++){
+		db.execute("INSERT INTO stockTBL (username, image, text) VALUES (?, ?, ?)", savedData[i].username, savedData[i].image, savedData[i].text);
+	}; 
 	
-	db.execute("INSERT INTO stockTBL (username, image, text) VALUES (?, ?, ?)", savedData.username, savedData.image, savedData.text);
+	//db.execute("INSERT INTO stockTBL (username, image, text) VALUES (?, ?, ?)", savedData.username, savedData.image, savedData.text);
 	console.log(savedData);
 	// var rowID = db.lastInsertRowId;
 	// console.log(rowID);
